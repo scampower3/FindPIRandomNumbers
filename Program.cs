3fa00f34cbfd19b3682dcaf4c8b6a49aa83d0693
Jun Quan Lee <leejunquan7@gmail.com>
2021-04-01 00:30:52 +0800

    Added checks for whole number intergers

--- Program.cs
+++ Program.cs
@@ -13,9 +13,16 @@ namespace FindPIRandomNumbers
             double pi; // Stores the value of Pi
             double inside=0; // stores the number of points inside the circle
             double total=0; // stores totol number of points generated
+            double points;//stores the number of points to generate
             Random random = new Random();
             Console.Write("This program will attempt to find the value of Pi using random numbers.\nThe higher the number of points, the more accurate the value of Pi will be.\nPlease input the number of points to generate:");
-            double points = int.Parse(Console.ReadLine()); //Retrieves the number of points to generate
+            while (!double.TryParse(Console.ReadLine(), out points) || points%1!=0) //Retrieves the number of points to generate and checks for int only
+            {
+                Console.Clear();
+                Console.Write("You have inserted an invalid number.\nPlease input the number of points to generate:");
+                
+            }
+               
             for(double i = 0; i<points; i++) // generates each point
             {
                 double x = random.NextDouble(); // generates random double between 0 and 1
